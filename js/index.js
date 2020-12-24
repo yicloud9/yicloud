ffcd59052b48bc01319c83838e2f657951d2a1ab
yicloud <zxlegend@163.com>
2020-12-24 09:57:47 +0800

    up

--- js/index.js
+++ js/index.js
@@ -412,7 +412,7 @@ const typedOptions = [
     }
     $scene1.fadeIn()
     loadingImage().then(()=> {
-        return
+        return 
         $scene1.fadeOut()
         setTimeout(()=> {
             $('#snow').fadeIn() 
